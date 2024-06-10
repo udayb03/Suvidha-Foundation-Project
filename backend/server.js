240854e72a9d5b50d57272e60944e87bc1bf4e0a
kirtan2003 <102241757+kirtan2003@users.noreply.github.com>
2024-06-10 12:53:13 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+// const router = express.Router();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API is running Successfully');
+})
+
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, console.log(`server started on port ${PORT}`));
